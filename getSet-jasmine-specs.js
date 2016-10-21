c3d748aab67803a45d22ca5226c643cb456ef4ee
nilsberge <nilsberge2@gmail.com>
2016-10-22 00:21:51 +1100

    Updated for new 'option' parameters

--- getSet-jasmine-specs.js
+++ getSet-jasmine-specs.js
@@ -85,115 +85,107 @@ describe("getSet function", function () {
         });
     });
 
-    describe("called with || option in propertyPath, and a setValue defined", function () {
+    describe("called with 'or' option in setValueOrOption, and an optionValue defined", function () {
         it("should get the value if the property already existed", function () {
             stubParentObject.foo = 123;
-            expect(getSet(stubParentObject, 'foo || ', 456)).toBe(123);
+            expect(getSet(stubParentObject, 'foo', 'or', 456)).toBe(123);
         });
         it("should set the property if it didn't exist", function () {
-            getSet(stubParentObject, 'bar || ', 789);
+            getSet(stubParentObject, 'bar', 'or', 789);
             expect(stubParentObject.bar).toBe(789);
         });
         it("should return the passed value if the property didn't exist", function () {
-            expect(getSet(stubParentObject, 'bar || ', 789)).toBe(789);
+            expect(getSet(stubParentObject, 'bar', 'or', 789)).toBe(789);
         });
     });
 
-    describe("called with ++ operator in propertyPath (without setValue)", function () {
+    describe("called with '++' operator in setValueOrOption (without optionValue)", function () {
         it("should increment the value of an undefined path to 1 and return the value", function () {
-            expect(getSet(stubParentObject, 'foo ++ ')).toBe(1);
-        });
-        it("should increment the value of a defined path by 1 and return the value", function () {
-            getSet(stubParentObject, 'foo ++ ');
-            expect(getSet(stubParentObject, 'foo ++ ')).toBe(2);
+            expect(getSet(stubParentObject, 'foo', '++')).toBe('++');
         });
     });
 
-    describe("called with -- operator in propertyPath (without setValue)", function () {
+    describe("called with '--' operator in setValueOrOption (without optionValue)", function () {
         it("should decrement the value of an undefined path to -1 and return the value", function () {
-            expect(getSet(stubParentObject, 'foo -- ')).toBe(-1);
-        });
-        it("should decrement the value of a defined path by 1 and return the value", function () {
-            getSet(stubParentObject, 'foo -- ');
-            expect(getSet(stubParentObject, 'foo -- ')).toBe(-2);
+            expect(getSet(stubParentObject, 'foo', '--')).toBe('--');
         });
     });
 
-    describe("called with ++ operator in propertyPath (with numeric setValue)", function () {
+    describe("called with '++' operator in setValueOrOption (with numeric optionValue)", function () {
         it("should set the value of an undefined path and return the value (Number)", function () {
-            expect(getSet(stubParentObject, 'foo ++ ', 3)).toBe(3);
+            expect(getSet(stubParentObject, 'foo', '++', 3)).toBe(3);
         });
         it("should increment the value of a defined path and return the value (Number)", function () {
             getSet(stubParentObject, 'foo', 3);
-            expect(getSet(stubParentObject, 'foo ++ ', 3)).toBe(6);
+            expect(getSet(stubParentObject, 'foo', '++', 3)).toBe(6);
         });
         it("should set the value of an undefined path and return the value (String)", function () {
-            expect(getSet(stubParentObject, 'foo ++ ', '3')).toBe(3);
+            expect(getSet(stubParentObject, 'foo', '++', '3')).toBe(3);
         });
         it("should increment the value of a defined path and return the value (String)", function () {
             getSet(stubParentObject, 'foo', '3');
-            expect(getSet(stubParentObject, 'foo ++ ', '3')).toBe(6);
+            expect(getSet(stubParentObject, 'foo', '++', '3')).toBe(6);
         });
     });
 
-    describe("called with -- operator in propertyPath (with numeric setValue)", function () {
+    describe("called with '--' operator in setValueOrOption (with numeric optionValue)", function () {
         it("should set the value of an undefined path and return the value (Number)", function () {
-            expect(getSet(stubParentObject, 'foo -- ', 3)).toBe(-3);
+            expect(getSet(stubParentObject, 'foo', '--', 3)).toBe(-3);
         });
         it("should decrement the value of a defined path and return the value (Number)", function () {
             getSet(stubParentObject, 'foo', -3);
-            expect(getSet(stubParentObject, 'foo -- ', 3)).toBe(-6);
+            expect(getSet(stubParentObject, 'foo', '--', 3)).toBe(-6);
         });
         it("should set the value of an undefined path and return the value (String)", function () {
-            expect(getSet(stubParentObject, 'foo -- ', '3')).toBe(-3);
+            expect(getSet(stubParentObject, 'foo', '--', '3')).toBe(-3);
         });
         it("should decrement the value of a defined path and return the value (String)", function () {
             getSet(stubParentObject, 'foo', '-3');
-            expect(getSet(stubParentObject, 'foo -- ', '3')).toBe(-6);
+            expect(getSet(stubParentObject, 'foo', '--', '3')).toBe(-6);
         });
     });
 
-    describe("called with ++ operator in propertyPath (with a non-numeric setValue)", function () {
+    describe("called with '++' operator in setValueOrOption (with a non-numeric optionValue)", function () {
         it("should throw a TypeError for setting that value on an undefined property", function () {
             expect(function () {
-                getSet(stubParentObject, 'foo ++ ', 'some string value');
+                getSet(stubParentObject, 'foo', '++', 'some string value');
             }).toThrowError(TypeError, "getSet: Could not in/decrement with value 'some string value' on property 'foo'. 'foo' is of type 'Undefined'.");
             expect(stubParentObject.foo).toBeUndefined();
         });
         it("should throw a TypeError for setting that value on an existing String property", function () {
             getSet(stubParentObject, 'foo', 'some string value');
             expect(function () {
-                getSet(stubParentObject, 'foo ++ ', 'some string value');
+                getSet(stubParentObject, 'foo', '++', 'some string value');
             }).toThrowError(TypeError, "getSet: Could not in/decrement with value 'some string value' on property 'foo'. 'foo' is of type 'String'.");
             expect(stubParentObject.foo).toBe('some string value');
         });
         it("should throw a TypeError for setting that value on an existing Number property", function () {
             getSet(stubParentObject, 'foo', 123);
             expect(function () {
-                getSet(stubParentObject, 'foo ++ ', 'some string value');
+                getSet(stubParentObject, 'foo', '++', 'some string value');
             }).toThrowError(TypeError, "getSet: Could not in/decrement with value 'some string value' on property 'foo'. 'foo' is of type 'Number'.");
             expect(stubParentObject.foo).toBe(123);
         });
     });
 
-    describe("called with -- operator in propertyPath (with a non-numeric setValue)", function () {
+    describe("called with '--' operator in setValueOrOption (with a non-numeric optionValue)", function () {
         it("should throw a TypeError for setting that value on an undefined property", function () {
             expect(function () {
-                getSet(stubParentObject, 'foo -- ', 'some string value');
+                getSet(stubParentObject, 'foo', '--', 'some string value');
             }).toThrowError(TypeError, "getSet: Could not in/decrement with value 'some string value' on property 'foo'. 'foo' is of type 'Undefined'.");
             expect(stubParentObject.foo).toBeUndefined();
         });
         it("should throw a TypeError for setting that value on an existing String property", function () {
             getSet(stubParentObject, 'foo', 'some string value');
             expect(function () {
-                getSet(stubParentObject, 'foo -- ', 'some string value');
+                getSet(stubParentObject, 'foo', '--', 'some string value');
             }).toThrowError(TypeError, "getSet: Could not in/decrement with value 'some string value' on property 'foo'. 'foo' is of type 'String'.");
             expect(stubParentObject.foo).toBe('some string value');
         });
         it("should throw a TypeError for setting that value on an existing Number property", function () {
             getSet(stubParentObject, 'foo', 123);
             expect(function () {
-                getSet(stubParentObject, 'foo -- ', 'some string value');
+                getSet(stubParentObject, 'foo', '--', 'some string value');
             }).toThrowError(TypeError, "getSet: Could not in/decrement with value 'some string value' on property 'foo'. 'foo' is of type 'Number'.");
             expect(stubParentObject.foo).toBe(123);
         });
